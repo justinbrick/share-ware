18fe99ca1886b4006e662742b514ec5a7ad80fa4
Justin <justin@justin.directory>
2024-08-19 21:48:49 -0400

    remove debug print

--- app/siteStorageMetric.tsx
+++ app/siteStorageMetric.tsx
@@ -8,9 +8,7 @@ export interface ISiteStorageMetricProps {
 }
 
 export default function SiteStorageMetric(props: ISiteStorageMetricProps) {
-
     const percent = (props.size / props.totalSize) * 100;
-    console.log(percent);
     return (
         <div className={`flex flex-row justify-between p-4 text-xl h-24 bg-slate-50 w-full items-center bg-opacity-35 ${props.className ?? ''}`} style={{
             backgroundImage: `linear-gradient(to right, rgb(3 252 44 / var(--tw-bg-opacity)) 0%, rgb(3 252 44 / var(--tw-bg-opacity)) ${percent}%, transparent ${percent}%, transparent 100%)`
