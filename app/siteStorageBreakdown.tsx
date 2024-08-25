3a5a7adace14481f7c6965824389507d1dd6e2f7
Justin <justin@justin.directory>
2024-08-24 22:45:34 -0400

    basic drive showing

--- app/siteStorageBreakdown.tsx
+++ app/siteStorageBreakdown.tsx
@@ -1,4 +1,7 @@
 import * as Graph from "@microsoft/microsoft-graph-types";
+import { useEffect, useState } from "react";
+import { getAuthFetch } from "./auth";
+import { humanFileSize } from "./util";
 
 export interface ISiteStorageBreakdownProps {
   /**
@@ -11,8 +14,45 @@ export default function SiteStorageBreakdown(
   props: ISiteStorageBreakdownProps
 ) {
   const { site } = props;
+  const [lists, setLists] = useState<Graph.Drive[]>([]);
+
+  useEffect(() => {
+    setLists([]);
+    if (!site) {
+      return;
+    }
+
+    const authFetch = getAuthFetch();
+
+    const getLists = async () => {
+      const response = await authFetch.fetch(
+        `https://graph.microsoft.com/v1.0/sites/${site.id}/drives?$select=id,name,webUrl,quota`
+      );
+      const drives = (await response.json()).value as Graph.Drive[];
+
+      setLists(
+        drives.sort((a, b) => (a.quota?.used ?? 0) - (b.quota?.used ?? 0))
+      );
+    };
+
+    getLists();
+  }, [site]);
+
   if (!site) {
     return <h1>Click on a site to continue.</h1>;
   }
-  return <div className="flex flex-col items-center"></div>;
+  return (
+    <div className="flex flex-col items-center w-full">
+      <h1 className="text-4xl p-4 text-left w-full">{site.displayName}</h1>
+      <ul className="w-full bg-gray-50">
+        {lists.map((drive) => (
+          <li key={drive.id} className="shadow-sm hover:bg-gray-100">
+            <a href={drive.webUrl!} className="block w-full p-8">
+              {drive.name} - {humanFileSize(drive.quota?.used ?? 0, false, 2)}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
